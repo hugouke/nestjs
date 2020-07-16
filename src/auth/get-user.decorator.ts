ac2d92b17a377b6fc8f40b950317e144f7a72177
Hugo Nogueira <hnogueira@DEV00125.funcional.local>
2020-07-16 09:29:49 -0300

    Get User Decorator

--- src/auth/get-user.decorator.ts
+++ src/auth/get-user.decorator.ts
@@ -1,6 +1,6 @@
-import { createParamDecorator } from "@nestjs/common";
+import { createParamDecorator, ExecutionContext } from "@nestjs/common";
 import { User } from "./user.entity";
 
-export const GetUser = createParamDecorator((data, req): User => {
-    return req.user;
+export const GetUser = createParamDecorator((data, req: ExecutionContext): User => {
+    return req.switchToHttp().getRequest().user;
 });
